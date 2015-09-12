ea27d20bd60777272cc0bbc03c6864505210e461
Nicholas Tesija <ntesija@umich.edu>
2015-09-12 19:45:39 -0400

    Updated For Joe
    Has some style?

--- expediaInject.js
+++ expediaInject.js
@@ -7,9 +7,11 @@ var textnode = document.createTextNode("EXPEDIA.... DOT COM!");
 node.appendChild(textnode);
 btn.appendChild(t);
 
+//var expediaResult = getExpediaResults(location, startDate, endDate);
+
 if (!isLoaded) {
 	var x = $("#event_summary").find("div");
-	x.find("ul").append("<li><center><h1>EXPEDIA</h1><p><table style = 'width:100%; text-align:center'><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr></table></p></center></li>");
+	x.find("ul").append("<li><font-size='12px' face='helvtica''>EXPEDIA</font><p><center><table style = 'width:100%; text-align:center'><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr></table></p></center></li>");
 	console.log("Appeneded li");
 	isLoaded = true;
 }
