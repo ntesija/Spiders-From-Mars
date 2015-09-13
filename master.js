ada87886054d80b4f078bd59da491f1f5acd213f
jobryan205 <jobryan205@gmail.com>
2015-09-13 08:56:06 -0400

    master.js update

--- master.js
+++ master.js
@@ -1,107 +1,107 @@
-var Expedia{}(
-	var hotelApiUrl = "http://terminal2.expedia.com/x/hotels";
-	var packageApiUrl = "http://terminal2.expedia.com/x/packages";
-	var airportApiUrl = "http://terminal2.expedia.com/x/geo/features?within=50km&"; //make distance configurable later on
-	var expPubKey = "37bjvD22imdEkcidS2Uc6bZ6Qe2uGimr";
-	var sitaPubKey = "e5d59a09c547bc478101c152c707ea30";
-	var xmlhttp = new XMLHttpRequest();
-	var airportResp; //To get the closest (largest) airports to arrv and org
-	var packageResp; //Get the best package deals
-	var userLat;
-	var userLon;
-	)(Expedia);
+var numPackages = 3;
+var packageApiUrl = "http://terminal2.com/x/packages";
+var expPubKey = "37bjvD22imdEkcidS2Uc6bZ6Qe2uGimr";
+var sitaPubKey = "e5d59a09c547bc478101c152c707ea30";
+var xmlhttp = new XMLHttpRequest();
+var airportResp; //To get the closest (largest) airports to arrv and org
+var packageResp; //Get the best package deals
+var userLat;
+var userLon;
 
 //Get user's location after the window loads
 window.onload = function() {
 	var startPos;
 	var geoSuccess = function(position) {
 		startPos = position;
-		Expedia.userLat = startPos.coords.latitude;
-	    Expedia.userLon = startPos.coords.longitude;
-	  };
-	  navigator.geolocation.getCurrentPosition(geoSuccess);
-	}
-}
+		userLat = startPos.coords.latitude;
+    userLon = startPos.coords.longitude;
+	};
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+}; //window.onload
 
-//1) Extracting data from the Facebook Event Page DOM
-var startDate = new Date(), endDate = new Date(), locationString = "";
+
+
 $( document ).ready(function() {
-	console.log( 'ready!' );
-	//var startDate = $("span[itemprop = 'startDate']").attr("content");
-	var dateString = $('._3slj ._6a ._5xhp').text();
-	locationString = $('._3xd0 ._6a ._5xhp').text();
-	console.log(locationString);
-	console.log(dateString);
+  //load Google Maps API
 
-	var now = new Date();
-	if (dateString.includes("to")) {
+  /*var script = document.createElement( 'script' );
+  script.src =
+      'http://maps.googleapis.com/maps/api/js' +
+      '?sensor=false&callbak=initMap';
+  document.body.appendChild( script );*/
+  
+
+  //1) Extracting data from the Facebook Event Page DOM
+  var startDate = new Date(), endDate = new Date(), now = new Date(), locationString = "";
+  console.log( 'ready!' );
+  //var startDate = $("span[itemprop = 'startDate']").attr("content");
+  var dateString = $('._3slj ._6a ._5xhp').text();
+  locationString = $('._3xd0 ._6a ._5xhp').text();
+  if (dateString.includes("to")) {
         console.log("this");
-		var splitDateStrings = dateString.split("to");
-		startDate = new Date(splitDateStrings[0].substring(0, splitDateStrings[0].indexOf('at')) + startDate.getFullYear());
-		endDate = new Date(splitDateStrings[1].substring(0, splitDateStrings[1].indexOf('at')) + endDate.getFullYear());
-	} else {
-		startDate = $("span[itemprop = 'startDate']").attr("content");
-		startDate = new Date(startDate);
-		//endDate = startDate;
+  	var splitDateStrings = dateString.split("to");
+  	startDate = new Date(splitDateStrings[0].substring(0, splitDateStrings[0].indexOf('at')) + startDate.getFullYear());
+  	endDate = new Date(splitDateStrings[1].substring(0, splitDateStrings[1].indexOf('at')) + endDate.getFullYear());
+  } else {
+  	startDate = $("span[itemprop = 'startDate']").attr("content");
+  	startDate = new Date(startDate);
+  	//endDate = startDate;
     endDate.setDate(startDate.getDate());
-	}
-	console.log(startDate);
-	console.log(endDate);
-  });
+  }
 
-//2) Expedia API calls
-var airports = getAirports(locationString);
-var jsonResponse = getBestPackageData(airports, startDate, endDate);
-//3) Parse API call results
-var jsonObj = JSON.parse(jsonResponse);
+  //2) Expedia API calls
+  var jsonObj = getBestPackageData(getAirports(locationString), startDate, endDate);
+  //3) Parse API call results
 
-//Package data
-var totalPrice = jsonObj.PackageSearchResultList.PackageSearchResult.PackagePrice.TotalPrice.Value;
-var packageDetailsURL = jsonObj.PackageSearchResultList.PackageSearchResult.DetailsUrl;
+  //Package data
+  /*var numberOfLayovers = [jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment.length - 1, 
+                        jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[1].FlightSegment.length - 1];
+  var departureDateTime =  jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment[0].DepartureDateTime;
+  var returnDateTime = jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[1].FlightSegment[numberOfLayovers[1]].ArrivalDateTime;*/
+  var departureDateTime =  new Date();
+  var returnDateTime = new Date();
+  var totalPrice = {}, packageDetailsURL = {};
+  /*for(i = 0; i < numPackages; i++){
+  totalPrice[i] = jsonObj[i].PackageSearchResultList.PackageSearchResult.PackagePrice.TotalPrice.Value;
+  packageDetailsURL[i] = jsonObj.PackageSearchResultList.PackageSearchResult.DetailsUrl;
+  }*/
+  //var totalPrice = jsonObj.PackageSearchResultList.PackageSearchResult.PackagePrice.TotalPrice.Value;
+  //var packageDetailsURL = jsonObj.PackageSearchResultList.PackageSearchResult.DetailsUrl;
 
-//Flight data
-var departureAirportCode = jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment[0].DepartureAirportCode;
-var departureDateTime =  jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment[0].DepartureDateTime;
-var numberOfLayovers = jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment.length - 1;
-var arrivalAirportCode = jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment[numberOfLayovers].ArrivalAirportCode;
-var arrivalDateTime =  jsonObj.FlightList.Flight.FlightItinerary.FlightLeg[0].FlightSegment[numberOfLayovers].ArrivalDateTime;
-var flightPrice = jsonObj.FlightList.Flight.FlightPrice.TotalRate.Value;
 
-//Hotel data
-var checkInDate = new Date(jsonObj.HotelList.CheckInDate);
-var checkOutDate = new Date(jsonObj.HotelList.CheckOutDate);
-var nightsInHotel = checkOutDate - checkInDate; //TODO: revise
-var nameOfHotel = jsonObj.HotelList.Hotel.Name;
-var hotelStarRating = jsonObj.HotelList.Hotel.StarRating;
-var hotelPrice = jsonObj.HotelList.Hotel.HotelPrice.TotalRate.Value;
+  //4) Insert Expedia Data into DOM
 
-//4) Insert Expedia Data into DOM
+  //removing toReturn array
+  //var toReturn = ["bliiiiing", "", ""];
+  //toReturn = [startDate, endDate, locationString];
 
-//removing toReturn array
-//var toReturn = ["bliiiiing", "", ""];
-//toReturn = [startDate, endDate, locationString];
+  console.log("injecting"); 
+  isLoaded = false;
 
-console.log("injecting"); 
-isLoaded = false;
+  //var expediaResult = getExpediaResults(location, startDate, endDate);
 
-//var expediaResult = getExpediaResults(location, startDate, endDate);
+  //var city = toReturn[2];
 
-var city = toReturn[2];
+  //toReturn[0].setDate(toReturn[0].getDate()-1);
+  //toReturn[1].setDate(toReturn[1].getDate()+1);
+  var start = departureDateTime.getMonth() + "-" + departureDateTime.getDate() + '-' + departureDateTime.getFullYear();
+  var end = returnDateTime.getMonth() + "-" + returnDateTime.getDate() + '-' + returnDateTime.getFullYear();
+  var price = ['$', '$$', '$$$'];
+  var url = ['https://www.google.com', 'https://www.google.com', 'https://www.google.com'];
 
-//toReturn[0].setDate(toReturn[0].getDate()-1);
-//toReturn[1].setDate(toReturn[1].getDate()+1);
-//var start = toReturn[0].getMonth() + "-" + toReturn[0].getDate() + '-' + toReturn[0].getFullYear();
-//var end = toReturn[1].getMonth() + "-" + toReturn[1].getDate() + '-' + toReturn[1].getFullYear();
-var price = ['$', '$$', '$$$'];
-var url = ['https://www.google.com', 'https://www.google.com', 'https://www.google.com'];
-
-var codeIn = "<li class='expander'><p><center><table style='width:85%' class='titleInfo'><tr><td>Travel to " + city + " from " + departureDateTime + " to " + arrivalDateTime + "</td></tr></table><table style='width:85%' class='info hide'><tr><td> Price: " + totalPrice + " </td><td><div align='right'><form action = '" + packageDetailsURL +"'><input type='submit' value = 'Book Now!'></form></div></td></tr><tr><td> Price: " + price[1] + " </td><td><div align='right'><form action = '" + url[1] + "'><input type='submit' value = 'Book Now!'></form></div></td></tr><tr><td> Price: " + price[2] + " </td><td><div align='right'><form action = '" + url[2] + "'><input type='submit' value = 'Book Now!'></form></div></td></tr></table></p></center><script>var clickArea=document.getElementsByClassName('titleInfo')[0];clickArea.addEventListener('click', function(){var dropDown=document.getElementsByClassName('info')[0];dropDown.classList.toggle('hide');})</script></li>"
-if (!isLoaded) {
+  var codeIn = "<li class='expander'><p><center><table style='width:85%' class='titleInfo'><tr><td>Travel to " + locationString + " from " + start + " to " + end + "</td></tr></table><table style='width:85%' class='info hide'><tr><td> Price: " + totalPrice[0] + " </td><td><div align='right'><form action = '" + packageDetailsURL[0] +"'><input type='submit' value = 'Book Now!'></form></div></td></tr><tr><td> Price: " + totalPrice[1] + " </td><td><div align='right'><form action = '" + packageDetailsURL[1] + "'><input type='submit' value = 'Book Now!'></form></div></td></tr><tr><td> Price: " + totalPrice[2] + " </td><td><div align='right'><form action = '" + packageDetailsURL[2] + "'><input type='submit' value = 'Book Now!'></form></div></td></tr></table></p></center><script>var clickArea=document.getElementsByClassName('titleInfo')[0];clickArea.addEventListener('click', function(){var dropDown=document.getElementsByClassName('info')[0];dropDown.classList.toggle('hide');})</script></li>"
+  if (!isLoaded) {
     var x = $("#event_summary").find("div");
     x.find("ul").append(codeIn);
     console.log("Appeneded li");
     isLoaded = true;
-}
+  }
+
+  });//document.ready
+
+
+
+
 
 
 /*
@@ -166,6 +166,7 @@ function getAirports(destination){
 function getGeocode(destination) {
 	var latlng;
 	var doneReading = false;
+  var geocoder = new google.maps.Geocoder();
 	geocoder.geocode({'address': destination}, function(results, status) {
 	if (status === google.maps.GeocoderStatus.OK) {
 		latlng = results[0].geometry.location;
@@ -210,7 +211,7 @@ function getBestPackageData(airports, startDate, endDate){
 			//Put together the package request for each pair of airports. Limit package to top result only
 			var currentPairRequestUrl = packageRequestUrl + departString + "&originAirport=" + originPort.code;
 			currentPairRequestUrl += "&destinationAirport=" + destPort.code + "&returnDate=" + returnString;
-			currentPairRequestUrl += "&limit=" + packageLimit + "&apikey=" + Expedia.expPubKey;
+			currentPairRequestUrl += "&limit=" + packageLimit + "&apikey=" + expPubKey;
 
 			var packageXmlHttp = newXMLHttpRequest(); //This could be an issue if callback isn't executed by the time the inner loop finishes execution
 			packageXmlHttp.onreadystatechange = function(){
