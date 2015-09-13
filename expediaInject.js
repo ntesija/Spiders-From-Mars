94a14710c2100cd482431391709430c68d3aa7e9
Nicholas Tesija <ntesija@umich.edu>
2015-09-12 20:27:45 -0400

    CSS Styles

--- expediaInject.js
+++ expediaInject.js
@@ -1,18 +1,11 @@
 console.log("injecting"); 
 isLoaded = false;
-var btn = document.createElement("BUTTON");
-var t = document.createTextNode("CLICK ME");
-var node = document.createElement("LI");
-var textnode = document.createTextNode("EXPEDIA.... DOT COM!");
-node.appendChild(textnode);
-btn.appendChild(t);
 
 //var expediaResult = getExpediaResults(location, startDate, endDate);
 
 if (!isLoaded) {
 	var x = $("#event_summary").find("div");
-	x.find("ul").append("<li><font-size='12px' face='helvtica''>EXPEDIA</font><p><center><table style = 'width:100%; text-align:center'><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr></table></p></center></li>");
+	x.find("ul").append("<li class = 'expander'><p><center><table style='width:85%' class='titleInfo'><tr><td>EXPEDIA</td></tr></table><table style='width:85%' class='info hide'><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr><tr><td> City </td><td> Day In </td><td> Day Out </td><td> Price: $ </td></tr></table></p></center><script>var clickArea=document.getElementsByClassName('titleInfo')[0];clickArea.addEventListener('click', function(){var dropDown=document.getElementsByClassName('info')[0];dropDown.classList.toggle('hide');})</script></li>");
 	console.log("Appeneded li");
 	isLoaded = true;
 }
-	
